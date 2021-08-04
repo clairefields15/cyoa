6f7a5a98b6c04a358bb3f82e9f9f30a03791e1ee
Claire Fields <clairefields15@gmail.com>
2021-08-04 13:40:55 -0600

    Add Route to favorites list

--- src/App/App.js
+++ src/App/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Main } from '../Main/Main';
 import { ErrorComponent } from '../ErrorComponent/ErrorComponent';
+import { Favorites } from '../Favorites/Favorites';
 
 export const App = () => {
   return (
     <Switch>
       <Route exact path='/' render={() => <Main />} />
+
+      <Route exact path='/favorites' render={() => <Favorites />} />
       <Route
         render={() => (
           <ErrorComponent errorMessage="Sorry that page doesn't exist, would you like to go home?" />
---
+++ src/Favorites/Favorites.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Favorites = () => {
+  return (
+    <>
+      <h2>Your Favorites</h2>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </>
+  );
+};
