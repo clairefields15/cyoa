3e8594e8e7bf5d00883b4fc80781e0440633c903
Claire Fields <clairefields15@gmail.com>
2021-08-04 13:38:45 -0600

    Add NavLink to go home from error page

--- src/ErrorComponent/ErrorComponent.js
+++ src/ErrorComponent/ErrorComponent.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export const ErrorComponent = ({ errorMessage }) => {
-  return <h2>{errorMessage}</h2>;
+  return (
+    <>
+      <h2>{errorMessage}</h2>
+      <NavLink to='/'>Home</NavLink>
+    </>
+  );
 };
