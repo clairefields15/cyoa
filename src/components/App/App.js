9fc02f3701bf731136012956ee3566184f61952c
Claire Fields <clairefields15@gmail.com>
2021-08-04 19:19:04 -0600

    Add scroll to top fn

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Main } from '../Main/Main';
 import { ErrorComponent } from '../ErrorComponent/ErrorComponent';
 import { Favorites } from '../Favorites/Favorites';
+import { ScrollToTop } from '../../helper-fns/ScrollToTop';
 
 export const App = () => {
+  const [allCities, setAllCities] = useState([]);
+
+  useEffect(() => {}, []);
+
   return (
     <Switch>
+      <ScrollToTop />
       <Route exact path='/' render={() => <Main />} />
 
       <Route exact path='/favorites' render={() => <Favorites />} />
