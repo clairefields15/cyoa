67dc874433ca60f8adba03b974959630659139f5
Claire Fields <clairefields15@gmail.com>
2021-08-04 18:50:59 -0600

    Remove unneeded files/variables

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Main } from '../Main/Main';
 import { ErrorComponent } from '../ErrorComponent/ErrorComponent';
 import { Favorites } from '../Favorites/Favorites';
-import PropTypes from 'prop-types';
 
 export const App = () => {
-  const [loading, setLoading] = useState(true);
-
   return (
-    // if loading show loading, if loaded, show main
-
     <Switch>
       <Route exact path='/' render={() => <Main />} />
 
