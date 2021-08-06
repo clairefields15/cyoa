4bdf93a136245395f68616864789a8d32d147919
Claire Fields <clairefields15@gmail.com>
2021-08-06 17:53:10 -0600

    Style message container 8ball

--- src/components/8Ball/8Ball.js
+++ src/components/8Ball/8Ball.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import eightBallFilled from '../../images/eightBallFilled.png';
 import eightBallBLUE from '../../images/eightBallBLUE.png';
 import PropTypes from 'prop-types';
@@ -6,12 +6,37 @@ import './8Ball.css';
 
 export const EightBall = ({ cityName }) => {
   const [clicked, setClicked] = useState(false);
+  const [message, setMessage] = useState('HELLO');
+
+  const handleClick = e => {
+    e.preventDefault();
+    setClicked(!clicked);
+    //fetch from 8ball API
+    //uppercase message
+    // set returned message in state
+    // conditionally render the message in the blue ball
+  };
+
+  // const handleClick = useCallback(
+  //   e => {
+  //     e.preventDefault();
+  //     setClicked(!clicked);
+  //   },
+  //   [clicked]
+  // );
 
   return (
     <section className='magic-8-ball'>
       {!clicked && <img src={eightBallFilled} alt='Magic 8 ball' />}
-      {clicked && <img src={eightBallBLUE} alt='Magic 8 ball' />}
-      <button onClick={() => setClicked(!clicked)}>
+      {clicked && (
+        <>
+          <img src={eightBallBLUE} alt='Magic 8 ball' />
+          <div className='message-container'>
+            <p className='message'>{message}</p>
+          </div>
+        </>
+      )}
+      <button onClick={e => handleClick(e)}>
         Should I move to {cityName}?
       </button>
     </section>
