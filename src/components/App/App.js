e1b029fcb9dd063bb8b4b6b0240074cdd456dace
Claire Fields <clairefields15@gmail.com>
2021-08-07 11:10:05 -0600

    Add nav bar and three buttons, change depending on url

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, NavLink, useLocation } from 'react-router-dom';
 import { Main } from '../Main/Main';
 import { ErrorComponent } from '../ErrorComponent/ErrorComponent';
 import { Favorites } from '../Favorites/Favorites';
@@ -9,6 +9,7 @@ import { Logo } from '../Logo/Logo';
 import './App.css';
 
 export const App = () => {
+  const { pathname } = useLocation();
   const [allCities, setAllCities] = useState([]);
   const [cityName, setCityName] = useState('');
   const [cityDetails, setCityDetails] = useState({});
@@ -62,32 +63,47 @@ export const App = () => {
     <>
       <ScrollToTop />
       <Logo />
+      <nav>
+        <button>Remove</button>
+        {pathname === '/' ? (
+          <NavLink to='/favorites' exact activeClassName='hidden-link'>
+            Favorites
+          </NavLink>
+        ) : (
+          <NavLink to='/' exact activeClassName='hidden-link'>
+            Explore
+          </NavLink>
+        )}
+        <button>Add</button>
+      </nav>
       {!errorMessage && isLoading && <h2>Loading...</h2>}
       {!!errorMessage && !isLoading && (
         <ErrorComponent errorMessage={errorMessage} />
       )}
       {!errorMessage && !isLoading && (
-        <Switch>
-          <Route
-            exact
-            path='/'
-            render={() => (
-              <Main
-                cityName={cityName}
-                cityDetails={cityDetails}
-                cityImage={cityImage}
-              />
-            )}
-          />
+        <>
+          <Switch>
+            <Route
+              exact
+              path='/'
+              render={() => (
+                <Main
+                  cityName={cityName}
+                  cityDetails={cityDetails}
+                  cityImage={cityImage}
+                />
+              )}
+            />
 
-          <Route exact path='/favorites' render={() => <Favorites />} />
+            <Route exact path='/favorites' render={() => <Favorites />} />
 
-          <Route
-            render={() => (
-              <ErrorComponent errorMessage="Sorry that page doesn't exist, would you like to go home?" />
-            )}
-          />
-        </Switch>
+            <Route
+              render={() => (
+                <ErrorComponent errorMessage="Sorry that page doesn't exist, would you like to go home?" />
+              )}
+            />
+          </Switch>
+        </>
       )}
     </>
   );
