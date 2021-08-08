99a75b064722ff984dc7b26476d27ef29751552c
Claire Fields <clairefields15@gmail.com>
2021-08-07 18:53:33 -0600

    Add heart to modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import likeRed from '../../images/likeRed.png';
 import './Modal.css';
 
 export const Modal = () => {
-  return <div className='modal'>ADDED TO FAVS</div>;
+  return (
+    <div className='modal'>
+      City added to favorites!
+      <img src={likeRed} alt='filled heart' className='in-favs' />
+    </div>
+  );
 };
