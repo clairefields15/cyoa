b93e282308ebc96e39c17506ec89e50eb1d06752
Claire Fields <clairefields15@gmail.com>
2021-08-09 16:59:38 -0600

    Restyle logo, add spinning compass

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Modal.css';
-import loading from '../../images/loading.gif';
-import logo from '../../images/logo.png';
+
+import spinningLogo from '../../images/compass-spin.gif';
 
 export const Modal = ({ message }) => {
   return (
     <div className='modal'>
       <div className='logo-container-modal'>
-        <img
-          src={logo}
-          alt='compass logo'
-          className='logo-img'
-          id='logoModal'
-        />
         <h2>CYOA</h2>
       </div>
       {message}
-      <img src={loading} alt='loading...' className='loading-dots' />
+      <img src={spinningLogo} alt='compass logo' className='spinning-logo' />
     </div>
   );
 };
